a950f5ebc04164dfd6eb1ffeba60f936efd7dc0e
Samuelgamondi <samigamondi@gmail.com>
2023-01-26 22:58:46 -0300

    Fix conexion email

--- form.php
+++ form.php
@@ -11,4 +11,14 @@ $mensaje = "Su email es " . $email . ",\r\n" ;
 $para = 'samuelgamondi01@gmail.com';
 $asunto = 'Este email fue enviado desde la web';
 
+#Usa la libreria PHPMailer para enviar el mail, para esto se necesita un servior con APACHE y esta libreria instalada. Ademas se necesita un servidor de email que por defecto lo busca en LocalHost:25 .  
+$cabeceras = 'From: '.$email."\r\n".
+'Reply-To: '.$email."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+if(mail($para, $asunto, $mensaje, $cabeceras)){
+    echo "El correo fue enviado correctamente";
+}else{
+    echo "Hubo un problema al enviar el correo";
+}
 ?>
